45dfeeeac0ce5af96658f320ef73e2c1ac451557
Bladimir <bladimir4hernandez@gmail.com>
2025-07-18 09:47:56 -0500

    Correccion

--- app.py
+++ app.py
@@ -7,15 +7,16 @@ car_data = pd.read_csv("notebooks/vehicles_us.csv")
 
 car_data = pd.read_csv('vehicles_us.csv') # leer los datos
 
+st.header('Análisis de vehículos en EE.UU.')
+
 st.write("Selecciona una o más visualizaciones para explorar el dataset de vehículos usados.")
 
-st.header('Análisis de vehículos en EE.UU.')
-button = st.checkbox('Mostrar datos')
+button = st.checkbox('Histograma del kilometraje')
 
 if button:
     fig= px.histogram(car_data, x="odometer", nbins=50, title='Histograma del kilometraje de vehículos')
 
-    st.write("Histograma de la variable 'odometer':")
+    #st.write("Histograma de la variable 'odometer':")
     st.plotly_chart(fig)
     
 st.divider()
@@ -27,3 +28,4 @@ if another_button:
     
     st.write("Gráfico de dispersión del año vs. kilometraje:")
     st.plotly_chart(fig2)
+    
